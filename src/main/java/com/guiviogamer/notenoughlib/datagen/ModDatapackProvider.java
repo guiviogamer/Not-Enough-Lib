5e57126c69ec9866d8b9d63f2fe0098b56857485
guiviogamer <gmviolante@gmail.com>
2026-03-18 16:01:29 +0000

    Important Changes

---
+++ src/main/java/com/guiviogamer/notenoughlib/datagen/ModDatapackProvider.java
@@ -0,0 +1,26 @@
+package com.guiviogamer.notenoughlib.datagen;
+
+import com.guiviogamer.notenoughlib.NotEnoughLib;
+import com.guiviogamer.notenoughlib.worldgen.ModBiomeModifiers;
+import com.guiviogamer.notenoughlib.worldgen.ModConfiguredFeatures;
+import com.guiviogamer.notenoughlib.worldgen.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(NotEnoughLib.MOD_ID));
+    }
+}
