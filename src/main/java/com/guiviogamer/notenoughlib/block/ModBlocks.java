85df915bd89d9e85231f337badeef0705b53357b
guiviogamer <gmviolante@gmail.com>
2026-04-02 18:29:59 +0100

    Important Changes

--- src/main/java/com/guiviogamer/notenoughlib/block/ModBlocks.java
+++ src/main/java/com/guiviogamer/notenoughlib/block/ModBlocks.java
@@ -38,7 +38,6 @@ public class ModBlocks {
                             .requiresCorrectToolForDrops()
                             .sound(SoundType.DEEPSLATE)));
 
-
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
